71c4bba15778445c8ba5c2fc30fa8a1000f53191
Roman Sutormin <rsutormin@lbl.gov>
2014-02-04 12:33:12 -0800

    First version of NCBI gbk-files parser.

---
+++ src/us/kbase/genomecomparison/gbk/EntityDescription.java
@@ -0,0 +1,19 @@
+package us.kbase.genomecomparison.gbk;
+
+/**
+ * @author rsutormin
+ */
+public class EntityDescription {
+	public String getSqlTableName() {
+		return null;
+	}
+	public String getSqlTableCreateQuery() {
+		return null;
+	}
+	public String getTypeManagerResource() {
+		return null;
+	}
+	public String getSqlTypeTableName() {
+		return null;
+	}
+}
---
+++ src/us/kbase/genomecomparison/gbk/GbkCallback.java
@@ -0,0 +1,14 @@
+package us.kbase.genomecomparison.gbk;
+
+import java.util.List;
+
+/**
+ * Results of parsing will be pushed back through implementation of this class.
+ * @author rsutormin
+ */
+public interface GbkCallback {
+	public void addHeader(String contigName, String headerType, List<GbkSubheader> items) throws Exception;
+    public void addFeature(String contigName, String featureType, int strand, int start, int stop, List<GbkLocation> locations, List<GbkQualifier> props) throws Exception;
+    public void setGenome(String genomeName, int taxId) throws Exception;
+    public void addSeqPart(String contigName, int seqPartIndex, String seqPart, int commonLen) throws Exception;
+}
